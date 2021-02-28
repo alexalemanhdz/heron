1da92c6fb998151619256a1d85a7de686259a8c1
alexalemanhdz <alex.aleman.public@gmail.com>
2021-02-27 18:43:17 -0600

    Fixed route bug

---
+++ src/containers/App/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <p>Hola mundo</p>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './containers/App';
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <p>Hola mundo</p>,
+  <App />,
   rootElement,
 );
