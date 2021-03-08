670dfa3c3c7d8f35bb3edbdc86d2bef762277cf3
alexalemanhdz <alex.aleman.public@gmail.com>
2021-03-07 22:09:34 -0600

    Working state

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -26,15 +26,15 @@ const Map = () => {
 
     L.heatLayer(crimeCoords).addTo(mapRef.current);
 
-  //   L.Routing.control({
-  //     waypoints: [
-  //       L.latLng(19.325874, -99.164856),
-  //       L.latLng(19.328115, -99.16230999999999),
-  //     ],
-  //     routeWhileDragging: true,
-  //     geocoder: L.Control.Geocoder.nominatim(),
-  //   }).addTo(mapRef.current);
-  // }, []);
+    L.Routing.control({
+      waypoints: [
+        L.latLng(19.325874, -99.164856),
+        L.latLng(19.328115, -99.16230999999999),
+      ],
+      routeWhileDragging: true,
+      geocoder: L.Control.Geocoder.nominatim(),
+    }).addTo(mapRef.current);
+  }, []);
 
   return <Container id="map" />;
 };
