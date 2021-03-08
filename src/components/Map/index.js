48a185124bc46d247405e3423799fb67be18022f
alexalemanhdz <alex.aleman.public@gmail.com>
2021-03-07 21:54:16 -0600

    Added leaflet-routing-machine

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,8 +1,10 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import L from 'leaflet';
 import 'leaflet.heat';
+import 'leaflet-routing-machine';
 import { crimeCoords } from '../../data/crimeCoords';
 
 const Container = styled.div`
@@ -22,6 +24,13 @@ const Map = () => {
     }).addTo(mapRef.current);
 
     L.heatLayer(crimeCoords).addTo(mapRef.current);
+
+    // L.Routing.control({
+    //   waypoints: [
+    //     L.latLng(19.325874, -99.164856),
+    //     L.latLng(19.328115, -99.16230999999999),
+    //   ],
+    // }).addTo(mapRef.current);
   }, []);
 
   return <Container id="map" />;
