b18b948b729471cc79147ba92bbd7218ac503ff4
alexalemanhdz <alex.aleman.public@gmail.com>
2021-03-13 13:58:36 -0600

    Added redux devtools

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import appReducer from './containers/App/reducers';
 
 import App from './containers/App';
 
-const store = createStore(appReducer);
+const store = createStore(appReducer, composeWithDevTools());
 
 const rootElement = document.getElementById('root');
 
