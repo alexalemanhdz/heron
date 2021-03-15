5ef3cea681512ff120b5881ac13eeaf867fbdcb7
alexalemanhdz <alex.aleman.public@gmail.com>
2021-03-15 14:22:26 -0600

    Removed redux -saga

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import appReducer from './containers/App/reducers';
-import appSaga from './containers/App/sagas';
 import App from './containers/App';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(appReducer, composeWithDevTools(
-  applyMiddleware(sagaMiddleware),
-));
+const store = createStore(appReducer, composeWithDevTools());
 const rootElement = document.getElementById('root');
-
-sagaMiddleware.run(appSaga);
 
 ReactDOM.render(
   <Provider store={store}>
